3c7a4f92df7efe2067b7701f9d94b260c6b73676
Mesfer <mesfer.vip@gmail.com>
2019-12-16 19:52:02 +0300

    change schema

--- app/models/user.js
+++ app/models/user.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
 
   
-  firstName:{type:String, required:true},
-  lastName:{type:String, required:true},
-  age:{type:Number, required:true},
+  firstName:{type:String},
+  lastName:{type:String},
+  age:{type:Number},
   email: {
     type: String,
     required: true,
