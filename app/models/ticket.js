c7ef27d0e5b64d7294653e2dcfd413560e2f4955
Mesfer <mesfer.vip@gmail.com>
2019-12-16 20:07:15 +0300

    create new model ticket

---
+++ app/models/ticket.js
@@ -0,0 +1,15 @@
+// // requir NPM 
+// const mongoose = require('mongoose')
+
+// const ticketSchema = new mongoose.Schema({
+
+
+
+// })
+
+
+// ticketSchema.virtual('user', {
+//     ref: 'User',
+//     localField: '_id',
+//     foreignField: 'orginzer'
+//   });
